4f85897cc5230d79893b8614c7ebe47e51184680
Adam Maziakowski <adam.maziakowski@recruitee.com>
2024-08-21 21:22:54 +0200

    refactor validate-it example

--- examples/module1/lesson1/validate-it/index.js
+++ examples/module1/lesson1/validate-it/index.js
@@ -1,31 +1,34 @@
+/*
+* Założenia:
+* Liczba musi być:
+* 1. większa od 0
+* 2. mniejsza od 100
+* 3. być podzielna przez 2 (parzysta)
+*
+*
+* Aplikacja nie działa poprawnie, ponieważ input.value jest interpretowany jako string
+* */
+function isValueValid(value) {
+  if (typeof value !== 'number') return false;
+  return value > 0 && value < 100 && value % 2 === 0;
+}
+
 function validator() {
   const input = document.getElementById('input');
-  const button = document.getElementById('button');
-  const button2 = document.getElementById('button2');
+  const validateButton = document.getElementById('button');
+  const clearButton = document.getElementById('button2');
   const result = document.getElementById('result');
 
-  button.addEventListener('click', () => {
-    if (input.value) {
-      if (Number.isInteger(input.value)) {
-        if (
-          Number(input.value) > 0 &&
-          Number(input.value) < 100 &&
-          Number(input.value) % 2 === 0
-        ) {
-          result.innerHTML = 'Valid';
-        } else {
-          result.innerHTML = 'Invalid';
-        }
-        result.innerHTML = 'Valid';
-      } else {
-        result.innerHTML = 'Invalid';
-      }
+  validateButton.addEventListener('click', () => {
+    const inputValue = Number(input.value);
+    if (inputValue && Number.isInteger(inputValue) && isValueValid(inputValue)) {
+      result.innerHTML = 'Valid';
     } else {
       result.innerHTML = 'Invalid';
     }
   });
 
-  button2.addEventListener('click', () => {
+  clearButton.addEventListener('click', () => {
     input.value = '';
     result.innerHTML = '';
   });
